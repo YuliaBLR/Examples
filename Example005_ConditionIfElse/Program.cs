baf47cb86321d284ec15fa1b1e978f38ffa77572
Yuliya <yulia.lukscha@yandex.ru>
2023-07-01 19:11:51 +0300

    add Exemple005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "юлия")
+{
+    Console.WriteLine("Ура, это же Юля!");
+}
+else
+{
+   Console.Write("Привет, "); 
+   Console.WriteLine(username); 
+}
